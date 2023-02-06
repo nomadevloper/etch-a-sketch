61797a593cfe98cca6001063cc1ea4ba4a60aa24
nomadevloper <122037674+nomadevloper@users.noreply.github.com>
2023-02-07 00:04:00 +0900

    Add buttons to change pixels per side and reset

--- app.js
+++ app.js
@@ -1,42 +1,57 @@
+const changePixel = document.querySelector(".changePixel");
+const reset = document.querySelector(".reset");
 const container = document.querySelector(".container");
-
-// for (let i=0; i<16; i++) {
-//     const row = document.createElement("div");
-//     row.classList.add(`row${i}`);
-//     for (let j=0; j<16; j++) {
-//         const col = document.createElement("div");
-//         col.classList.add(`col${j}`);
-//         row.appendChild(col);
-//     }
-//     container.appendChild(row);
-// }
+const defaultNum = 16;
 
 let mouseDown = false;
 
-
-for (let i=0; i<16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row", `row${i}`);
-    for (let j=0; j<16; j++) {
-        const col = document.createElement("div");
-        col.classList.add("grid", `col${j}`);
-        col.addEventListener("mousedown", () => {
-            mouseDown = true;
-        });
-        col.addEventListener("mouseup", ()=> {
-            mouseDown = false;
-        })
-        col.addEventListener("click", function(){
-            this.classList.add("clicked");
-        });
-        col.addEventListener("mouseover", function(){
-            if(mouseDown) {
-                this.classList.add("clicked");
-            }
-        });
-        row.appendChild(col);
+function makeGrids(n) {
+    for (let i=0; i<n; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for (let j=0; j<n; j++) {
+            const grid = document.createElement("div");
+            grid.classList.add("grid");
+            grid.addEventListener("mousedown", () => {
+                mouseDown = true;
+            });
+            grid.addEventListener("mouseup", ()=> {
+                mouseDown = false;
+            })
+            grid.addEventListener("click", changeColor);
+            grid.addEventListener("mouseover", changeColor);
+            row.appendChild(grid);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
 
-const grids = document.querySelectorAll(".grid");
+function removeGrids() {
+    const rows = document.querySelectorAll(".row");
+    rows.forEach(row => {
+        row.remove();
+    });
+}
+
+function changeColor(e) {
+    if (e.type==="click" || mouseDown) {
+        this.classList.add("clicked");
+    }
+}
+
+changePixel.addEventListener("click", function(){
+    let num;
+    do {
+        num = prompt("Enter the number of squares per side for new grid. *max=100");
+        num = Number.parseInt(num);
+    } while(!(num && num<=100))
+    removeGrids();
+    makeGrids(num);
+})
+
+reset.addEventListener("click", function(){
+    removeGrids();
+    makeGrids(defaultNum);
+})
+
+makeGrids(defaultNum);
